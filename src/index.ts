941f2ec5f0c8b40d43f22f631ee53342ce3f816b
Github GPG acces <erdnaxela02@hotmail.fr>
2025-04-26 18:02:04 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -162,7 +162,7 @@ async function removeEmptyCreatedNetwork(docker: Docker, containerId: string) {
   for (const network of dragonifyNetworks) {
     logger.info(`444444444444444444444 All Network are ${dragonifyNetworks}`)
     logger.info(`444444444444444444444 All Network are ${network.Containers}`)
-    if (network.Containers.length !== undefined) {
+    if (network.Containers.length === 0) {
       logger.info(`5555555555555555 All Network are ${network.Containers}`)
     }
   }
